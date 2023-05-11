48fa4305da00c210d6dd32d1fd5b5da6e6cd7d0a
jdu214843 <214843a@jdu.uz>
2023-05-11 12:40:27 +0500

    React card components

--- src/App.js
+++ src/App.js
@@ -19,17 +19,39 @@ class Card extends React.Component {
       }     
       return (
        <React.Fragment>
+        <>
+        <div className="card-container">
         <div className="card-1">
-          <img src={img} alt='images'/>
-          <h2 className='title'>{this.state.title}</h2>
-          <button onClick={clickBtn}>{this.state.btn}</button>
+            <img src={img} alt='images'/>
+            <h2 className='title'>{this.state.title}</h2>
+            <h3 className='title'>{this.state.describe}</h3>
+            <button onClick={clickBtn}>{this.state.btn}</button>
+          </div>
+          <div className="card-2">
+            <img src={img} alt='images'/>
+            <h2 className='title'>{this.state.title}</h2>
+            <button onClick={clickBtn}>{this.state.btn}</button>
+          </div>
+          <div className="card-3">
+            <img src={img} alt='images'/>
+          </div>
+          <div className="card-4">
+            <img src={img} alt='images'/>
+          </div>
+          <div className="card-5">
+            <img src={img} alt='images'/>
+          </div>
+          <div className="card-6">
+            <img src={img} alt='images'/>
+          </div>
+          <div className="card-7">
+            <img src={img} alt='images'/>
+          </div>
+          <div className="card-8">
+            <img src={img} alt='images'/>
+          </div>
         </div>
-        <div className="card-2">
-          <img src={img} alt='images'/>
-        </div>
-        <div className="card-3">
-          <img src={img} alt='images'/>
-        </div>
+        </>
        </React.Fragment>
     )
   };
--- src/index.js
+++ src/index.js
@@ -6,8 +6,12 @@ import Card from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Card />
-    
+    <Card
+      title="Welcome"
+      describe= 'to Webbrain'
+      imgUrl="./img/images.jpg"
+    />
+   
     
   </React.StrictMode>
 );

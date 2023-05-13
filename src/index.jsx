d2047b4c2da87af6266b502f8179d025f6fe8dfe
jdu214843 <214843a@jdu.uz>
2023-05-13 13:18:37 +0500

    react 3-dars table homework

--- src/index.jsx
+++ src/index.jsx
@@ -11,33 +11,53 @@ class Student extends React.Component {
             data: student,
         }
     }
+
     render() {
+        const onFilter = (e)=>{
+            let result = student.filter(value => value.name.includes(e.target.value));
+            this.setState({data: result});
+        }
+        const onDelete = (e)=>{
+            let result = student.filter(value => value.id !== e.target.value);
+            this.setState({data: result});
+        }
         return(
             <div>
+                <input className='filter' type="text" onChange={onFilter} placeholder="filter" />
+                <table className='wrapper'>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>NAME</th>
+                            <th>STATUS</th>
+                            <th>EDIT</th>
+                            <th>DELETE</th>
+                            <th>SELECTED</th>
+                        </tr>
+                    </thead>
+                <tbody className='tbody'>
                 {
                     this.state.data.map(({id, name, status}) =>{
                         return (
-                           <div className='wrapper'>
-                             <div className='parent-wrap'>
-                                <table className='table'>
-                                    <tbody className='tbody'>
-                                        <tr className='table-tr'>
-                                            <th className='name-id'>{id}</th>
-                                            <th className='data-name'>{name}</th>
-                                            <th className='status'>{status}</th>
-                                        </tr>
-                                    </tbody>
-                                    <div className='btn-wrap'>
-                                        <button>edit</button>
-                                        <button>delete</button>
-                                        <button>selecet</button>
-                                    </div>
-                                </table>
-                            </div>
-                           </div>
+                            <tr key={id}>
+                                <td className='name-id'>{id}</td>
+                                <td>{name}</td>
+                                <td>{status}</td>
+                                <td>
+                                    <button>Edit</button>
+                                </td>
+                                <td>
+                                    <button>Delete</button>
+                                </td>
+                                <td>
+                                    <button>Selected</button>
+                                </td>
+                            </tr>
                         )
                     })
                 }
+                </tbody>
+                </table>
             </div>
 
         )

fc4311d34e5c32eea6dae9675685c36d4dfb8e31
jdu214843 <214843a@jdu.uz>
2023-05-13 10:51:38 +0500

    react 2 dars card homework

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import Card from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Card
-      title="Welcome"
-      describe= 'to Webbrain'
-      imgUrl="./img/images.jpg"
-    />
+    <div className='items'>
+     <Card
+        title="Welcome"
+        describe= 'to Webbrain'
+        imgUrl="./img/images.jpg"
+      />
+    </div>
    
     
   </React.StrictMode>
